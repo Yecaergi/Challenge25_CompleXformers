346ec98fe74627f8173887880a929fa398712cf5
Yecaergi <167825732+Yecaergi@users.noreply.github.com>
2025-04-01 08:55:26 -0300

    Update train_model.py
    - We successfully evaluated your entry, which received a score of 0.058 on the validation set using the Challenge evaluation metric. 
    
    -With my validation set
    score 0.075
    AUROC 0.591
    AUPRC 0.040
    Accuracy 0.970
    F-measure 0.000

--- train_model.py
+++ train_model.py
@@ -1,17 +1,5 @@
-#!/usr/bin/env python
-
-# Please do *not* edit this script. Changes will be discarded so that we can train the models consistently.
-
-# This file contains functions for training your model for the Challenge. You can run it as follows:
-#
-#   python train_model.py -d data -m model -v
-#
-# where 'data' is a folder containing the Challenge data, 'model' is a folder for saving your model, and , and -v is an optional
-# verbosity flag.
-
 import argparse
 import sys
-
 from helper_code import *
 from team_code import train_model
 
